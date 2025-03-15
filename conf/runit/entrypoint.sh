64b4463a15f8869fab161ed03a83fcd050b8e70a
m4d3bug <m4d3bug@gmail.com>
2025-03-16 00:31:40 +0800

    Update entrypoint.sh

--- conf/runit/entrypoint.sh
+++ conf/runit/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i "s|CSRF_TRUSTED_ORIGINS.*|CSRF_TRUSTED_ORIGINS = ['http://${CURRENT_IP}']|" webvirtcloud/settings.py
+sed -i "s|CSRF_TRUSTED_ORIGINS.*|CSRF_TRUSTED_ORIGINS = ['http://${CURRENT_IP}',]|" webvirtcloud/settings.py
 exec "$@"
